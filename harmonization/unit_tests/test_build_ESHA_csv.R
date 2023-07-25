400b08cc67e90fc01ab0af90b7977b00764cfcfe
Palomnyk <aaronyerke@gmail.com>
2023-07-25 14:58:18 -0400

    add unit testing

---
+++ harmonization/unit_tests/test_build_ESHA_csv.R
@@ -0,0 +1,48 @@
+# Author: Aaron Yerke (aaronyerke@gmail.com)
+# Script for testing lognorm impmentation
+# Test 1 uses the "FormwardReads" DADA2 output from Zeller
+
+####Defining functions -----------------------------------------------------####
+print("Defining functions")
+
+#Loading dependencies ------------------------------------------------------
+print("Loading dependencies")
+# --------------------------------------------------------------------------
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!requireNamespace("testthat")) install.packages("testthat")
+library("testthat")
+if (!requireNamespace("data.table", quietly = TRUE)) BiocManager::install("data.table")
+library("data.table")
+if (!requireNamespace("readxl", quietly = TRUE))  BiocManager::install("readxl")
+library("readxl")
+####Establishing directory layout and other constants-----------------------####
+print("Establishing directory layout and other constants.")
+data_dir <- file.path("nutition_data")
+
+
+####Set up constants--------------------------------------------------------####
+print("Setting up other constants")
+# --------------------------------------------------------------------------
+test_columns <- c("Day","Meal","Recipe","Item.Name")
+
+manual_day2 <- data.frame(data.table::fread(file = file.path(data_dir, 
+                                                             "MED training set.tsv"),
+                                            header=TRUE, data.table=FALSE))
+manual_day2 <- manual_day2[,test_columns]
+sample_df <- data.frame(data.table::fread(file = file.path(data_dir, 
+                                                               "combined_esha_studies.tsv"),
+                                              header=TRUE, data.table=FALSE))
+
+final_sample <- sample_df[sample_df$Day == "Day 2" 
+                                & sample_df$Study == "MED Diet Complete ESHA Analysis Spreadsheet.xlsx"
+                                & sample_df$Intervention == "TYPICAL AMERICAN", 
+                                test_columns]
+
+####Testing combined data---------------------------------------------------####
+test_that("Testing Day 2 of combined ESH",
+          { expect_equal(final_sample, manual_day2)
+            expect_equivalent(final_sample, manual_day2)
+            # expect_identical(lognorm(untransformed), confirmed_transformed)
+          })
+
+print("Completed script!")
