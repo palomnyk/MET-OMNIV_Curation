d4db39753a000c085208761f074e6ba3ce9e52b6
Palomnyk <aaronyerke@gmail.com>
2023-09-29 12:40:25 -0400

    Unit testing

--- harmonization/unit_tests/test_build_ESHA_csv.R
+++ harmonization/unit_tests/test_build_ESHA_csv.R
@@ -17,8 +17,8 @@ if (!requireNamespace("readxl", quietly = TRUE))  BiocManager::install("readxl")
 library("readxl")
 ####Establishing directory layout and other constants-----------------------####
 print("Establishing directory layout and other constants.")
-data_dir <- file.path("nutition_data")
-
+data_dir <- file.path("..","..", "nutition_data")
+print(getwd())
 
 ####Set up constants--------------------------------------------------------####
 print("Setting up other constants")
@@ -26,23 +26,26 @@ print("Setting up other constants")
 test_columns <- c("Day","Meal","Recipe","Item.Name")
 
 manual_day2 <- data.frame(data.table::fread(file = file.path(data_dir, 
-                                                             "MED training set.tsv"),
+                                                             "MED_training_set.tsv"),
                                             header=TRUE, data.table=FALSE))
 manual_day2 <- manual_day2[,test_columns]
+
+manual_day2 <- manual_day2[order(sort(manual_day2$Item.Name)),]
+
 sample_df <- data.frame(data.table::fread(file = file.path(data_dir, 
                                                                "combined_esha_studies.tsv"),
                                               header=TRUE, data.table=FALSE))
-
+#Subset data to make final sample version for testing
 final_sample <- sample_df[sample_df$Day == "Day 2" 
                                 & sample_df$Study == "MED Diet Complete ESHA Analysis Spreadsheet.xlsx"
                                 & sample_df$Intervention == "TYPICAL AMERICAN", 
                                 test_columns]
+final_sample <- final_sample[order(sort(final_sample$Item.Name)),]
 
 ####Testing combined data---------------------------------------------------####
 test_that("Testing Day 2 of combined ESH",
-          { expect_equal(final_sample, manual_day2)
-            expect_equivalent(final_sample, manual_day2)
-            # expect_identical(lognorm(untransformed), confirmed_transformed)
+          {
+           expect_equivalent(final_sample, manual_day2)
           })
 
 print("Completed script!")
