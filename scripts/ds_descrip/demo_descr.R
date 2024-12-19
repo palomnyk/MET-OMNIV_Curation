8808a4b6a15b5fc9f176173c42208be092f9a809
palomnyk@github.com <aaronyerke@gmail.com>
2024-12-19 11:35:12 -0600

    plotting scripts

---
+++ scripts/ds_descrip/demo_descr.R
@@ -0,0 +1,114 @@
+#Author: Aaron Yerke, aaronyerke@gmail.com
+#Script to post information about participants demographics. It should takea a
+#csv and make histograms for the ages and bmi, and a pie plot for age. One for 
+#each corrected site, plus a combined histogram for the entire study.
+#Requires df with all metadata - including demographic data.
+
+rm(list = ls()) #clear workspace
+
+#### Loading dependencies ####
+# if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!requireNamespace("ggplot2", quietly = TRUE)) install.packages("ggplot2")
+library("ggplot2")
+if (!requireNamespace("optparse", quietly = TRUE)) install.packages("optparse")
+library("optparse")
+# --------------------------------------------------------------------------
+print("Reading cml arguments")
+# --------------------------------------------------------------------------
+option_list <- list(
+  optparse::make_option(c("-a", "--data"), type="character",
+                        default="data/mapping/noMap_metadata_demo.csv",
+                        help="filename in tables folder"),
+  optparse::make_option(c("-l", "--delim"), type="character", default="\t",
+                        help="metadata file deliminator", metavar="character"),
+  optparse::make_option(c("-o", "--output_fpath"), type="character", 
+                        default="output/no_map/graphics/demo_plots.pdf",
+                        help="relative output file path"),
+  optparse::make_option(c("-d", "--output_dir"), type="character", 
+                        default="no_map",
+                        help="folder in output dir")
+); 
+
+opt_parser <- optparse::OptionParser(option_list=option_list);
+
+opt <- optparse::parse_args(opt_parser);
+
+print(opt)
+
+# --------------------------------------------------------------------------
+print("Establishing directory layout and other constants.")
+#### Establish directory layout and other constants ####
+output_label <- opt$output_fname
+output_dir <- file.path("output", opt$output_dir)
+dir.create(output_dir)
+dir.create(file.path(output_dir, "graphics"))
+dir.create(file.path(output_dir, "tables"))
+
+num_breaks <- 50
+
+#### Loading in data ####
+my_table <- read.csv(file = file.path(opt$data),
+                     header = T,
+                     check.names =F)
+
+my_table$sex <- as.character(gsub(1, "male", gsub(0, "female", my_table$sex)))
+
+num_columns <- c("age", "bmi", "sex")
+
+#### Organizing metadata ####
+# Remove rows that don't have corrected site
+my_table <- my_table[!is.na(my_table$CORRECTED_SITE),]
+my_table <- my_table[!is.na(my_table$sex),]
+
+pdf(file = file.path(opt$output_fpath),
+    width=10*length(num_columns), height=7*length(num_columns))
+
+par(mfrow=c(length(num_columns),5), cex.sub=3, mar=c(5,2,5,1),
+    cex=2)#oma=c(1,1,4,1)
+for (feat in num_columns){
+  cap_feat <- paste(toupper(substr(feat, 1, 1)), substr(feat, 2, nchar(feat)), sep="")
+  # par(mfrow=c(1,5), cex.sub=1.5, oma=c(1,1,4,1))
+  counts <- my_table[,feat]
+  no_na <- sum(!is.na(counts))
+  supstring <- paste("n:", no_na)
+  if (is.numeric(counts)){
+    hist(counts, main = paste("all sites"),
+         xlab = supstring, breaks=num_breaks)
+  }else{
+    # par(mar=c(20,4,4,4))
+    barplot(table(counts), main = paste("all sites"),
+            las = 3,
+            xlab = supstring)
+    # my_string <- paste(my_string,"\n",str(table(counts)))
+    # cat(str(data.frame(table(counts))), file = cat_out_name, append = T)
+  }
+  for (site in unique(my_table$CORRECTED_SITE)){
+    df <- my_table[my_table$CORRECTED_SITE == site,]
+    counts <- df[,feat]
+    no_na <- sum(!is.na(counts))
+    supstring <- paste("n:", no_na)
+    # print(supstring)
+    if (is.numeric(counts)){
+      hist(counts, main = paste(site),
+           xlab = supstring, breaks=num_breaks)
+    }else{
+      # par(mar=c(20,4,4,4))
+      barplot(table(counts), main = paste(site), las = 3,
+              xlab = supstring)
+      # my_string <- paste(my_string,"\n",str(table(counts)))
+      # cat(str(data.frame(table(counts))), file = cat_out_name, append = T)
+    }
+  }
+  mtext(cap_feat, side=3.1, line = 2.95, adj=-2.5, cex=3.5)#side=3 puts it at top
+}
+
+# agg_table <- aggregate(my_table$sex, by=list(my_table$CORRECTED_SITE), sum)
+
+
+# y: number of participants
+# x: site
+# stacked colors: sex
+
+dev.off()
+
+print("End of R script!")
---
+++ scripts/ds_descrip/exp_org_Sankey.R
@@ -0,0 +1,112 @@
+# Author: Aaron Yerke (aaronyerke@gmail.com)
+# Script for building sankey plots showing how each response feature shares top
+# X metabolomic elements by feature importance
+# Left nodes = respnse feature
+# Right nodes = metabolites
+#
+
+rm(list = ls()) #clear workspace
+
+print(paste("Working in", getwd()))
+
+#### Loading dependencies ####
+if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
+if (!requireNamespace("networkD3", quietly = TRUE))  BiocManager::install("networkD3")
+# Load package
+library("networkD3")
+print("Loaded packages")
+
+if (!requireNamespace("optparse", quietly = TRUE)) BiocManager::install("optparse")
+library("optparse")
+
+print("Loaded dependencies")
+source(file.path("scripts","data_org", "data_org_func.R"))
+
+#### Functions ####
+
+#### Parse command line arguments ####
+option_list <- list(
+  optparse::make_option(c("-f", "--input"), type="character",
+                        default="data/mapping/noMap_metadata_demo.csv",
+                        help="path of first csv"),
+  optparse::make_option(c("-s", "--output_dir"), type="character",
+                        default = "no_map", help="dir in /output"),
+  optparse::make_option(c("-o", "--out_name"), type="character",
+                        default = "exp_org_Sankey.html",
+                        help="Path of output csv.")
+);
+opt_parser <- optparse::OptionParser(option_list=option_list);
+opt <- parse_args(opt_parser);
+
+print("Commandline arguments:")
+print(opt)
+
+#### Establish directory layout and other constants ####
+output_dir <- file.path("output", opt$output_dir)
+dir.create(output_dir)
+
+accuracy_threshold <- 0.1
+top_X <- 5
+
+#### Loading in data ####
+inp_df <- read.csv(opt$input, check.names = FALSE,
+                     row.names = "PARENT_SAMPLE_NAME")
+inp_df <- inp_df[!is.na(inp_df$CORRECTED_SITE),]
+exp_org_cols <- c("CORRECTED_SITE", "TREATMENT", "beef_level")
+
+#### Org data ####
+
+#empty variables to fill in
+name_source <- character(0)
+name_target <- character(0)
+counts <- c()
+
+# Find top x elements for each resp feature, make links
+for (exp in 1:length(exp_org_cols)-1){
+  my_col <- exp_org_cols[exp]
+  column_1 <- inp_df[,my_col]
+  my_col2 <- exp_org_cols[exp+1]
+  column_2 <- inp_df[,my_col2]
+  for (link_start in unique(column_1)){
+    for(link_target in unique(column_2)){
+      count <- sum(inp_df[,my_col] == link_start &
+                     inp_df[,my_col2] == link_target)
+      if (count > 0){
+        col_count1 <- sum(column_1 == link_start)
+        col_count2 <- sum(column_2 == link_target)
+        name_source <- c(name_source, paste(c(link_start, col_count1), collapse = ": "))
+        name_target <- c(name_target, paste(c(link_target, col_count2), collapse = ": "))
+        counts <- c(counts, count)
+      }
+    }
+  }
+}
+
+node_names <- c(name_source, name_target)
+unique_nodes <- unique(node_names)
+
+target_index <- match(name_target, unique_nodes)
+target_index <- target_index - 1
+source_index <- match(name_source, unique_nodes)
+source_index <- source_index - 1
+
+
+links <- data.frame(source = source_index, target = target_index, 
+                    count = counts)
+nodes <- data.frame(nodes = unique_nodes)
+
+#update node names to clarify in plot
+p <- sankeyNetwork(Links = links, Nodes = nodes, Source = "source",
+                   Target = "target", Value = "count", NodeID = "nodes",
+                   units = " participants", fontSize = 14, nodeWidth = 30, 
+                   margin = list(50,100,500,100), sinksRight = TRUE,
+                   height = 500, width = 500)
+p
+
+# p <- htmlwidgets::prependContent(p, htmltools::tags$h1("Figure . Sankey plot showing metabolites that are important to each feature."))
+# # p <- htmlwidgets::appendContent(p, htmltools::tags$p("*, SR that included children/adolescents only; ~, SR that included only adults; all other SR included all age groups."))
+# # p <- htmlwidgets::appendContent(p, htmltools::tags$p("Higgins KA, Rawal R, Kramer M, Baer DJ, Yerke A, Klurfeld DM. An overview of reviews on the association of low calorie sweetener consumption with body weight and adiposity. Advances in Nutrition (accepted)."))
+# 
+saveNetwork(p, file=file.path(output_dir, "graphics", opt$out_name), selfcontained = TRUE)
+
+print("End R script.")
