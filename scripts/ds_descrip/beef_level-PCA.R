21330646eb666e3f97141ba0f31ddceabbc83169
palomnyk@github.com <aaronyerke@gmail.com>
2025-02-14 10:50:49 -0600

    add ellipsis

--- scripts/ds_descrip/beef_level-PCA.R
+++ scripts/ds_descrip/beef_level-PCA.R
@@ -21,7 +21,7 @@ output_dir <- file.path("output", "no_map_chick_is_not_beef")
 dir.create(file.path(output_dir, "graphics"))
 dir.create(file.path(output_dir, "tables"))
 
-metad_intrest <- c("beef_level")
+metad_intrest <- c("beef_level", "beef_level_oz")
 
 #### Loading in data ####
 mtbmcs_df <-openxlsx::read.xlsx(metabo_f,
@@ -45,7 +45,6 @@ meta_df <- meta_df[keep_rows,]
 mtbmcs_df <- mtbmcs_df[keep_rows,]
 
 meta_df <-  type.convert(meta_df, as.is = TRUE)#Reset column types automatically (for factors)
-
 
 print("Checking for columns/chems with zero variance")
 
@@ -72,7 +71,7 @@ my_order <- c("USDA-MED", "PSU-MED", "Purdue", "MB/IIT")
 meta_df$CORRECTED_SITE <- factor(meta_df$CORRECTED_SITE, levels = c(my_order))
 
 #### Make plots ####
-pdf(file.path(output_dir, "graphics", "beef_level_PCA.pdf"))
+pdf(file.path(output_dir, "graphics", "beef_level_PCA.pdf"), width = 6, height = 4)
 for(md in metad_intrest){
   # g <- ggplot2::ggplot(myPCA, aes(x=PC1, y=PC2, col = factor(meta_df[,md]))) +
   #   ggplot2::geom_point(aes(shape = factor(meta_df$CORRECTED_SITE, ))) +
@@ -80,7 +79,7 @@ for(md in metad_intrest){
   #   xlab(paste0("PC 1, ", round(my_var_exp[1],2)*100, "%")) +
   #   ylab(paste0("PC 2, ", round(my_var_exp[2],2)*100, "%")) +
   #   labs(color = "BEEF INTAKE", shape = "Site") +
-  #   theme(text=element_text(size=20))
+  #   theme(text=element_text(size=18))
   # print(g)
   meta_fact <- factor(meta_df[,md])
   t_pval_1 <- summary(aov(myPCA$PC1~meta_fact))[[1]][["Pr(>F)"]][[1]]#do anova and extract pvalue
@@ -89,13 +88,25 @@ for(md in metad_intrest){
   print(paste("PVAL PC2: ", t_pval_2))
   pc1_symb <- ifelse( t_pval_1 < 0.05, yes = "*", no = "")
   pc2_symb <- ifelse( t_pval_2 < 0.05, yes = "*", no = "")
-  g <- ggplot2::ggplot(myPCA, aes(x=PC1, y=PC2, col = meta_fact)) +
+  g <- ggplot2::ggplot(myPCA, aes(x=PC1, y=PC2, col = meta_fact, group=meta_fact)) +
     ggplot2::geom_point(aes(shape = factor(meta_df$CORRECTED_SITE))) +
     # ggplot2::ggtitle(paste0("PCA of Metabolomics")) + # Blank Title for the Graph
     ggplot2::xlab(paste0("PC1", pc1_symb, ", ", round(my_var_exp[1],2)*100, "%")) +
     ggplot2::ylab(paste0("PC2", pc2_symb, ", ", round(my_var_exp[2],2)*100, "%")) +
     ggplot2::labs(color = md, shape = "Site") +
-    ggplot2::theme(text=element_text(size=20))
+    ggplot2::theme(text=element_text(size=18)) +
+    stat_ellipse()
+  ggplot2::ggsave(g, device = "png",
+                  filename = file.path(output_dir, "graphics", paste0(md, "_PCA_elipse.png")))
+  print(g)
+  
+  g <- ggplot2::ggplot(myPCA, aes(x=PC1, y=PC2, col = meta_fact, group=meta_fact)) +
+    ggplot2::geom_point(aes(shape = factor(meta_df$CORRECTED_SITE))) +
+    # ggplot2::ggtitle(paste0("PCA of Metabolomics")) + # Blank Title for the Graph
+    ggplot2::xlab(paste0("PC1", pc1_symb, ", ", round(my_var_exp[1],2)*100, "%")) +
+    ggplot2::ylab(paste0("PC2", pc2_symb, ", ", round(my_var_exp[2],2)*100, "%")) +
+    ggplot2::labs(color = md, shape = "Site") +
+    ggplot2::theme(text=element_text(size=18)) +
   ggplot2::ggsave(g, device = "png",
                   filename = file.path(output_dir, "graphics", paste0(md, "_PCA.png")))
   print(g)
@@ -106,19 +117,32 @@ for(md in metad_intrest){
     ggplot2::xlab(paste0("PC1", pc1_symb)) +
     ggplot2::ylab(paste0(md)) +
     # ggplot2::labs(color = md, shape = "Site") +
-    ggplot2::theme(text=element_text(size=20))
+    ggplot2::theme(text=element_text(size=18))
   print(g)
   
   g <- ggplot2::ggplot(myPCA, aes(x=PC2, y=meta_fact)) +
     ggplot2::geom_boxplot() +
     ggplot2::ggtitle(paste0("Boxplot of PC2")) + # Blank Title for the Graph
-    ggplot2::xlab(paste0("PC1", pc1_symb)) +
+    ggplot2::xlab(paste0("PC2", pc1_symb)) +
     ggplot2::ylab(paste0(md)) +
     # ggplot2::labs(color = md, shape = "Site") +
-    ggplot2::theme(text=element_text(size=20))
+    ggplot2::theme(text=element_text(size=18))
   print(g)
   
 }
+
+g <- ggplot2::ggplot(myPCA, aes(x=PC1, y=PC2, col = meta_df[,"beef_level_oz"])) +
+  ggplot2::geom_point(aes(shape = factor(meta_df$CORRECTED_SITE))) +
+  # ggplot2::ggtitle(paste0("PCA of Metabolomics")) + # Blank Title for the Graph
+  ggplot2::xlab(paste0("PC1", pc1_symb, ", ", round(my_var_exp[1],2)*100, "%")) +
+  ggplot2::ylab(paste0("PC2", pc2_symb, ", ", round(my_var_exp[2],2)*100, "%")) +
+  ggplot2::labs(color = md, shape = "Site") +
+  ggplot2::theme(text=element_text(size=18)) +
+  ggplot2::scale_colour_gradient(low = "cornflowerblue", high = "deeppink4", na.value = NA)
+  # ggplot2::scale_colour_gradient(colors = heat.colors(2))
+  # heat.colors(n, alpha, rev = FALSE)
+  print(g)
+
 dev.off()
 
 print("End R script.")
