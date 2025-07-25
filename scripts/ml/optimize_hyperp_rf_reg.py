742abdb4bb780574ceec2d8ef5041760a1d751f4
palomnyk@github.com <aaronyerke@gmail.com>
2025-07-25 15:28:59 -0500

    properly save output

--- scripts/ml/optimize_hyperp_rf_reg.py
+++ scripts/ml/optimize_hyperp_rf_reg.py
@@ -9,6 +9,15 @@
 # 	1. https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestRegressor.html
 # 	2. https://scikit-learn.org/stable/modules/grid_search.html
 # 	3. https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html#gallery-examples
+# 	4. best parameters vs best estimators:
+#			Best parameter gives best model for your data
+#			Best estimators gives best model for next data
+#			Source: https://www.kaggle.com/discussions/questions-and-answers/377391
+
+# Outcome:
+# Best Parameters: {'criterion': 'poisson', 'max_depth': None, 'max_features': 'sqrt', 'min_samples_leaf': 1, 'min_samples_split': 5, 'n_estimators': 100}
+# Best Estimator: RandomForestRegressor(criterion='poisson', max_features='sqrt',
+#                       min_samples_split=5)
 
 # --------------------------------------------------------------------------
 print("Loading external libraries.",flush = True)
@@ -30,6 +39,7 @@ pathlib.Path(os.path.join(".",output_dir, "graphics")).mkdir(parents=True, exist
 pathlib.Path(os.path.join(".",output_dir, "tables")).mkdir(parents=True, exist_ok=True) 
 assert os.path.exists(output_dir)
 
+result_fpath = pathlib.Path(os.path.join(".",output_dir, "tables", "best_param.txt"))
 # --------------------------------------------------------------------------
 print("Establishing other constants.")
 # --------------------------------------------------------------------------
@@ -39,16 +49,9 @@ gs_parameters = {
 	"criterion": ["squared_error", "absolute_error", "friedman_mse", "poisson"],
 	"max_features": ("sqrt", "log2"),
 	"max_depth": [None, 10, 20],
-    "min_samples_split": [1, 2, 5],
+    "min_samples_split": [2, 5, 10],
     "min_samples_leaf": [1, 2, 10],}
 
-# gs_parameters = {
-# 	"n_estimators": (100, 500),
-# 	"criterion": ["squared_error", "poisson"],
-# 	"max_features": ("sqrt", "log2"),
-# 	"max_depth": [None, 10, 20],
-#     "min_samples_split": [1, 2, 5],
-#     "min_samples_leaf": [1, 2, 10],}
 expl_table_path = "data/metabolomics/demo-log-filt_all_bat_norm_imput-chem.csv"
 expl_df = pd.read_csv(os.path.join(".", expl_table_path), \
 		sep=",", header=0, index_col=id_var).fillna(0)
@@ -62,6 +65,7 @@ print("Organizing data.", flush = True)
 # --------------------------------------------------------------------------
 safe_ids = outcom_df.loc[outcom_df[outcom_var].notna(), id_var]
 intersect_safe_ids = list(set(safe_ids) & set(expl_df.index))
+print(f"Expl_df shape: {expl_df.shape} ")
 expl_df = expl_df.loc[expl_df.index.isin(safe_ids),:]
 expl_df = expl_df.round(decimals=4)
 
@@ -78,3 +82,18 @@ sorted(grid_search.cv_results_.keys())
 
 print("Best Parameters:", grid_search.best_params_)
 print("Best Estimator:", grid_search.best_estimator_)
+
+# --------------------------------------------------------------------------
+print("Saving output.", flush = True)
+# --------------------------------------------------------------------------
+
+#best_param_str = ", ".join(list(grid_search.best_params_))
+#best_esti_str = ", ".join(list(grid_search.best_estimator_))
+with open(result_fpath, "w+") as fl:
+	fl.write(str(grid_search.best_params_))
+	fl.write("\n")
+	fl.write(str(grid_search.best_estimator_))
+
+# --------------------------------------------------------------------------
+print(f"Completed {__file__}", flush = True)
+# --------------------------------------------------------------------------
