7d7d9d61191c1b37a167a8125d8348350ca10e57
palomnyk@github.com <aaronyerke@gmail.com>
2025-08-27 15:55:46 -0500

    Add option to only use subset.

--- scripts/ds_descrip/comb_output_Sankey.R
+++ scripts/ds_descrip/comb_output_Sankey.R
@@ -1,7 +1,7 @@
 # Author: Aaron Yerke (aaronyerke@gmail.com)
-# Script for building sankey plots showing how each response feature shares top
+# Script for building Sankey plots showing how each response feature shares top
 # X metabolomic elements by feature importance
-# Left nodes = respnse feature
+# Left nodes = response feature
 # Right nodes = metabolites
 
 rm(list = ls()) #clear workspace
@@ -27,24 +27,28 @@ source(file.path("scripts","data_org", "data_org_func.R"))
 #### Parse command line arguments ####
 option_list <- list(
   optparse::make_option(c("-f", "--featimp_path"), type="character",
-                        # default = "output/no_map_auto_protein/tables/feat_imp_all_sites-demo-log-filt_all_bat_norm_imput-chem-auto_protein.csv",
-                        default = "output/ml_eval/tables/reorg-chicken-shap_feat_imp-demo-log-filt_all_bat_norm_imput-chem-.csv",
+                        default = "output/norm_meat/tables/shap_feat_imp_MB_IIT-demo-log-filt_all_bat_norm_imput-chem-meats_rmOut_g_per_kg_bw.csv",
+                        # default = "output/ml_eval/tables/reorg-chicken-shap_feat_imp-demo-log-filt_all_bat_norm_imput-chem-.csv",
                         help="path of first csv"),
   optparse::make_option(c("-s", "--output_dir"), type="character",
                         default = "no_map", help="dir in /output"),
   optparse::make_option(c("-o", "--out_name"), type="character",
-                        default = "output/nomap/graphics/metabolites_Sankey.html",
+                        default = "output/no_map/graphics/test_metabolites_Sankey.html",
                         help="Path of output csv."),
   optparse::make_option(c("-t", "--accuracy_threshold"), type="numeric",
                         default = -1000000000,
                         help="Threshold for score for inclusion in graphic."),
   optparse::make_option(c("-g", "--grouping_column"), type="character",
-                        # default = "response_var",
-                        default = "SITE",
+                        default = "response_var",
+                        # default = "SITE",
                         help="Column for grouping - not features"),
   optparse::make_option(c("-x", "--top_X"), type="integer",
                         default = 5,
-                        help="Number of comparison features in plot.")
+                        help="Number of comparison features in plot."),
+  optparse::make_option(c("-r", "--group_subset"), type="character",
+                        default = FALSE,
+                        # default = "beef_rmOut_g_per_kg_bw,chicken_rmOut_g_per_kg_bw,pork_rmOut_g_per_kg_bw,turkey_rmOut_g_per_kg_bw,meat_rmOut_g_per_kg_bw",
+                        help="Array of acceptable grouping column variables as coma seperated string. Use if wanting to use a subset of the available grouping column variables")
 );
 opt_parser <- optparse::OptionParser(option_list=option_list);
 opt <- parse_args(opt_parser);
@@ -56,13 +60,25 @@ print(opt)
 output_dir <- file.path("output", opt$output_dir)
 dir.create(output_dir)
 
+plain_resp_vars <- c("beef","chicken","pork","turkey","meat")
+all_proc <- c("beef_proc","chicken_proc","pork_proc", "turkey_proc","meat_proc")
+all_mproc <- c("beef_min_proc","chicken_min_proc","pork_min_proc", "turkey_min_proc","meat_min_proc")
+
 accuracy_threshold <- opt$accuracy_threshold
 top_X <- opt$top_X
 
 #### Loading in data ####
 feat_imp <- read.csv(opt$featimp_path, check.names = FALSE)
-print(feat_imp)
+print(head(feat_imp))
+
 #### Org data ####
+##### Subset grouping column if needed ####
+if (opt$group_subset != FALSE){
+  my_subset <- unlist(strsplit(opt$group_subset, split = ","))
+  feat_imp <- feat_imp[feat_imp[,opt$grouping_column] %in% my_subset, ]
+}
+
+# Aggregate accuracies by grouping column
 ave_accuracies <- aggregate(feat_imp$accuracy, by=list(feat_imp[,opt$grouping_column,]), mean)
 #remove features below threshold
 ave_accuracies <- ave_accuracies[ave_accuracies$x > accuracy_threshold, ]
@@ -107,12 +123,12 @@ p <- sankeyNetwork(Links = links, Nodes = nodes, Source = "source",
                    Target = "target", Value = "count", NodeID = "nodes",
                    units = "Ave_score", fontSize = 14, nodeWidth = 30, 
                    margin = list(50,100,500,100), sinksRight = TRUE,
-                   height = 500, width = 500)
-p
+                   height = 500, width = 600)
 
 p <- htmlwidgets::prependContent(p, htmltools::tags$h1(sub('\\..[^\\.]*$', '', basename(opt$featimp_path))))
 # p <- htmlwidgets::appendContent(p, htmltools::tags$p("*, SR that included children/adolescents only; ~, SR that included only adults; all other SR included all age groups."))
 # p <- htmlwidgets::appendContent(p, htmltools::tags$p("Higgins KA, Rawal R, Kramer M, Baer DJ, Yerke A, Klurfeld DM. An overview of reviews on the association of low calorie sweetener consumption with body weight and adiposity. Advances in Nutrition (accepted)."))
+p
 
 print(paste("save network:", opt$out_name))
 saveNetwork(p, file=file.path(opt$out_name), selfcontained = TRUE)
@@ -124,59 +140,3 @@ webshot(url = opt$out_name,
         file = file.path(paste0(sub('\\..[^\\.]*$', '', opt$out_name),".pdf")))#remove extention and add .pdf ext
 
 print("End R script")
-
-#### Set options for reorg sites Chicken ####
-# optparse::make_option(c("-f", "--featimp_path"), type="character",
-#                       default = "/project/nhanes_ml/beef_biomarkers/output/ml_eval/tables/reorg-chicken-shap_feat_imp-demo-log-filt_all_bat_norm_imput-chem-.csv",
-#                       help="path of first csv"),
-# optparse::make_option(c("-s", "--output_dir"), type="character",
-#                       default = "ml_eval", help="dir in /output"),
-# optparse::make_option(c("-o", "--out_name"), type="character",
-#                       default = "Chicken_metabol_comp_sites_Sankey.html",
-#                       help="Path of output csv."),
-# optparse::make_option(c("-t", "--accuracy_threshold"), type="numeric",
-#                       default = -1000000000,
-#                       help="Threshold for score for inclusion in graphic."),
-# optparse::make_option(c("-g", "--grouping_column"), type="character",
-#                       default = "SITE",
-#                       help="Column for grouping - not features"),
-# optparse::make_option(c("-x", "--top_X"), type="integer",
-#                       default = 5,
-#                       help="Number of comparison features in plot.")
-
-#### Set options for reorg sites Avg_dystolic bp ####
-# optparse::make_option(c("-f", "--featimp_path"), type="character",
-#                       default = "/project/nhanes_ml/beef_biomarkers/output/ml_eval/tables/reorg-Avg_dystolic-shap_feat_imp-rf_demographics_meats--.csv",
-#                       help="path of first csv"),
-# optparse::make_option(c("-s", "--output_dir"), type="character",
-#                       default = "ml_eval", help="dir in /output"),
-# optparse::make_option(c("-o", "--out_name"), type="character",
-#                       default = "Avg_dystolic_demo_meat_comp_sites_Sankey.html",
-#                       help="Path of output csv."),
-# optparse::make_option(c("-t", "--accuracy_threshold"), type="numeric",
-#                       default = -1000000000,
-#                       help="Threshold for score for inclusion in graphic."),
-# optparse::make_option(c("-g", "--grouping_column"), type="character",
-#                       default = "SITE",
-#                       help="Column for grouping - not features"),
-# optparse::make_option(c("-x", "--top_X"), type="integer",
-#                       default = 5,
-#                       help="Number of comparison features in plot.")
-
-# optparse::make_option(c("-f", "--featimp_path"), type="character",
-#                       default = "/project/nhanes_ml/beef_biomarkers/output/ml_eval/tables/reorg-beef-shap_feat_imp-demo-log-filt_all_bat_norm_imput-chem-.csv",
-#                       help="path of first csv"),
-# optparse::make_option(c("-s", "--output_dir"), type="character",
-#                       default = "ml_eval", help="dir in /output"),
-# optparse::make_option(c("-o", "--out_name"), type="character",
-#                       default = "Chicken_metabol_comp_sites_Sankey.html",
-#                       help="Path of output csv."),
-# optparse::make_option(c("-t", "--accuracy_threshold"), type="numeric",
-#                       default = -1000000000,
-#                       help="Threshold for score for inclusion in graphic."),
-# optparse::make_option(c("-g", "--grouping_column"), type="character",
-#                       default = "SITE",
-#                       help="Column for grouping - not features"),
-# optparse::make_option(c("-x", "--top_X"), type="integer",
-#                       default = 7,
-#                       help="Number of comparison features in plot.")
