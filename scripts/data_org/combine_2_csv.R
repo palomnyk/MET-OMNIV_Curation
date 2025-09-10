e2265f93cb0dd73ecbc9172141052573e17ef971
palomnyk@github.com <aaronyerke@gmail.com>
2025-09-10 10:25:45 -0500

    combine can keep all or only one set of row names

--- scripts/data_org/combine_2_csv.R
+++ scripts/data_org/combine_2_csv.R
@@ -17,19 +17,22 @@ source(file.path("scripts","data_org", "data_org_func.R"))
 #### Parse command line arguments ####
 option_list <- list(
   optparse::make_option(c("-f", "--first_path"), type="character",
-                        # default = "Data/diet/d1_nutri_food_2015.csv",
                         default = "data/mapping/all_sites_meats.csv",
+                        # default = "data/mapping/Purdue-meats_g.csv",
                         help="path of first csv"),
   optparse::make_option(c("-s", "--secnd_path"), type="character",
                         # default = "Data/demo/helper_features_2009-2020.csv",
                         default = "data/mapping/all_sites_demo.csv",
                         help="Path of second csv."),
   optparse::make_option(c("-o", "--out_path"), type="character",
-                        default = "data/mapping/all_sites_metadata_auto-pro_demo.csv",
+                        default = "data/mapping/Purdue-meats_g-demo.csv",
                         help="Path of output csv."),
   optparse::make_option(c("-i", "--id_var"), type="character",
                         default="PARENT_SAMPLE_NAME",
-                        help="Path of second csv.")
+                        help="Path of second csv."),
+  optparse::make_option(c("-k", "--keep"), type="character",
+                        default="all",
+                        help="Options in merge function to keep all of both dfs (all) or all of first df (all.x)")
 );
 opt_parser <- optparse::OptionParser(option_list=option_list);
 opt <- parse_args(opt_parser);
@@ -44,15 +47,23 @@ dir.create(output_dir)
 #### Loading in data ####
 first_df <- read.csv(opt$first_path, header = T, check.names = F,
                      row.names=opt$id_var)
+print(paste("1st df nrow:", nrow(first_df)))
 secnd_df <- read.csv(opt$secnd_path, header = T, check.names = F,
                      row.names=opt$id_var)
+print(paste("2nd df nrow:", nrow(secnd_df)))
 print("Data loaded!")
 
-final_table <- merge(first_df, secnd_df, all = TRUE, by = 0)
+if (opt$keep == "all"){
+  final_table <- merge(first_df, secnd_df, all = TRUE, by = 0)
+}
+if (opt$keep == "all.x"){
+  final_table <- merge(first_df, secnd_df, all.x = TRUE, by = 0)
+}
+print(paste("final df nrow:", nrow(final_table)))
 
-# row.names(final_table) <- final_table[,"Row.names"]
-
+#### Designate selected column as row names ####
 final_table[,opt$id_var] <- final_table[,"Row.names"]
+# Drop it after making it into row names
 final_table <- final_table[,!names(final_table) %in% c("Row.names")]
 
 write.csv(final_table, file = opt$out_path, row.names = FALSE)
